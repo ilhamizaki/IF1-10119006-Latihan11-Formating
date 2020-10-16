753e6aaf21f16947746e2a9ed3a61025966057f1
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-16 09:20:24 +0700

    fix

---
+++ src/if1/pkg10119006/latihan11/IF110119006Latihan11.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119006.latihan11;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ilham zaki
+ * Kelas    : IF1
+ * NIM      : 10119006
+ * Deskripsi Program : Tampil Formatting
+ */
+public class IF110119006Latihan11 {
+
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
